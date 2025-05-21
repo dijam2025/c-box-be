1aa00399ab8fed0fde2a851d2d62a9bc35b81d18
eongeung <iyoungheuni@gmail.com>
2025-05-22 02:46:23 +0900

    feat : Add error message

---
+++ src/main/java/dijam/c_box_be/rental/controller/RentalAdviceController.java
@@ -0,0 +1,21 @@
+package dijam.c_box_be.rental.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class RentalAdviceController {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> handleIllegalArgument(IllegalArgumentException ex) {
+        return ResponseEntity.badRequest().body(Map.of("message", ex.getMessage()));
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<Map<String, String>> handleIllegalState(IllegalStateException ex) {
+        return ResponseEntity.badRequest().body(Map.of("message", ex.getMessage()));
+    }
+}
