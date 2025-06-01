b3386d5bd7b6015fffab4c4417af2f1f938ce68f
eongeung <iyoungheuni@gmail.com>
2025-06-02 04:08:04 +0900

    feat: Modify bug

---
+++ src/main/java/dijam/c_box_be/rental/entity/Item.java
@@ -0,0 +1,18 @@
+package dijam.c_box_be.rental.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long itemId;
+
+    private String item;
+    private boolean rented;
+}
