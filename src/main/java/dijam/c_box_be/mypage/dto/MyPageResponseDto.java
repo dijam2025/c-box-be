10c692f52e7414ac0cc7e26a65c4dc54532bf17f
eongeung <iyoungheuni@gmail.com>
2025-06-04 06:12:50 +0900

    feat: Add MyPageResponseDto

--- src/main/java/dijam/c_box_be/mypage/dto/MyPageResponseDto.java
+++ src/main/java/dijam/c_box_be/mypage/dto/MyPageResponseDto.java
@@ -1,4 +1,10 @@
 package dijam.c_box_be.mypage.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class MyPageResponseDto {
+    private String name;
 }
