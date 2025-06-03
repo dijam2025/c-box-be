f070823f6f0e1e4688547f68056b712763982ad9
eongeung <iyoungheuni@gmail.com>
2025-06-04 06:12:17 +0900

    feat: Add MyPageRentalDto

--- src/main/java/dijam/c_box_be/mypage/dto/MyPageRentalDto.java
+++ src/main/java/dijam/c_box_be/mypage/dto/MyPageRentalDto.java
@@ -1,4 +1,17 @@
 package dijam.c_box_be.mypage.dto;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
 public class MyPageRentalDto {
+    private Long itemId;
+    private String item;
+    private LocalDateTime rentedAt;
+    private LocalDateTime dueDate;
+    private LocalDateTime returnedAt;
+    private long daysLeft;
+    private String statusMessage;
 }
